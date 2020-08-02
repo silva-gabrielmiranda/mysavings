bcfb72aecf6d23441c8bfe82ff963fd5ac314a29
unknown <silva.gabrielmiranda@gmail.com>
2020-08-02 17:01:59 -0300

    tela inclulsao

--- client/mysavings/src/App.js
+++ client/mysavings/src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import { Grid, TextField, Typography, Paper, FormControl, InputAdornment, Input, MenuItem, Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import './App.css';
 
+const useStyles = makeStyles((theme) => ({
+	root: {
+		flexGrow: 1,
+	},
+	margins: {
+		margin: 10
+	}
+}));
+
+const valueType = [
+	{ value: 1, description: "Receita" },
+	{ value: 2, description: "Despesa" }
+]
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	const classes = useStyles();
+	const [values,setValues] = useState({
+		type: "",
+		amount: ""
+	})
+
+	const handleChange = field => event => setValues({...values, [field] : event.target.value})
+
+	const handleSubmit = () => console.log(values);
+
+	return (
+		<Grid container justify="center" className={classes.root} alignItems="center">
+			<Grid item xs={5}>
+				<Paper elevation={2}>
+					<Typography align="center" gutterBottom variant="h6">Adicione uma nova despesa ou receita</Typography>
+					<Grid container justify="center" alignItems="center">
+						<Grid item xs={8}>
+							<FormControl fullWidth>
+								<TextField label="Tipo" select onChange={handleChange("type")} value={values.type} className={classes.margins}>
+									{valueType.map(item => <MenuItem key={item.value} value={item.value}>{item.description}</MenuItem>)}
+								</TextField>
+							</FormControl>
+						</Grid>
+						<Grid item xs={8}>
+							<FormControl fullWidth>
+								<Input
+									id="standard-adornment-amount"
+									startAdornment={<InputAdornment position="start">R$</InputAdornment>}
+									value = {values.amount}
+									onChange = {handleChange("amount")}
+									placeholder="150,00"
+									className={classes.margins}
+								/>
+							</FormControl>
+						</Grid>
+						<Grid item xs={8}>
+							<Button fullWidth className={classes.margins} onClick={handleSubmit}>Adicionar</Button>
+						</Grid>
+					</Grid>
+				</Paper>
+			</Grid>
+			<Grid item xs={12}></Grid>
+		</Grid>
+	);
 }
 
 export default App;
--- client/mysavings/src/index.js
+++ client/mysavings/src/index.js
@@ -2,16 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { blue } from '@material-ui/core/colors';
+import { CssBaseline } from '@material-ui/core';
+
+let themePreference = true;
+
+const darkTheme = createMuiTheme({
+  palette: {
+    type: themePreference ? "dark" : "light",
+    primary: blue,
+  },
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
