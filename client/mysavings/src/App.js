ffd2bb3ffe12de2ba896c6760793ee043c5206ce
unknown <silva.gabrielmiranda@gmail.com>
2020-08-15 16:58:18 -0300

    adicionado valor e quantidade da ação, atualizado Schema

--- client/mysavings/src/App.js
+++ client/mysavings/src/App.js
@@ -1,12 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Grid, TextField, Typography, Paper, FormControl, InputAdornment, Input, MenuItem, Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import './App.css';
 import axios from 'axios';
+import moment from 'moment';
 
 const useStyles = makeStyles((theme) => ({
 	root: {
 		flexGrow: 1,
+		marginTop: 20
 	},
 	margins: {
 		marginBottom: 10
@@ -14,8 +16,8 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const valueType = [
-	{ value: 1, description: "Receita" },
-	{ value: 2, description: "Despesa" }
+	{ value: 1, description: "Compra" },
+	{ value: 2, description: "Venda" }
 ]
 
 function App() {
@@ -23,10 +25,23 @@ function App() {
 	const classes = useStyles();
 	const [values, setValues] = useState({
 		type: "",
-		amount: ""
+		amount: "",
+		price: "",
+		description: ""
 	})
+	const [data, setData] = useState([{ description: "teste" }])
+
+	useEffect(() => {
+		getAllData();
+		return () => console.log("Clear Function")
+	}, []);
+
+	const getAllData = async () => {
+		await axios.post("http://localhost:3001/getAllData").then(resp => setData(resp.data.allSavingsLog)).catch(err => console.error(err))
+	}
 
 	const handleChange = field => event => setValues({ ...values, [field]: event.target.value })
+
 
 	const handleSubmit = () => axios.post("http://localhost:3001/newData", values).then(response => console.log(response.data)).catch(err => console.error(err));
 
@@ -34,22 +49,37 @@ function App() {
 		<Grid container justify="center" className={classes.root} alignItems="center">
 			<Grid item xs={5}>
 				<Paper elevation={2}>
-					<Typography align="center" gutterBottom variant="h6" className={classes.margins}>Adicione uma nova despesa ou receita</Typography>
+					<Typography align="center" gutterBottom variant="h6" className={classes.margins}>Adicione uma ação</Typography>
 					<Grid container justify="center" alignItems="center">
 						<Grid item xs={8}>
 							<FormControl fullWidth>
 								<Input
 									startAdornment={<InputAdornment position="start">R$</InputAdornment>}
-									value={values.amount}
-									onChange={handleChange("amount")}
-									placeholder="150,00"
+									value={values.price}
+									onChange={handleChange("price")}
+									placeholder="Valor"
 									className={classes.margins}
 								/>
 							</FormControl>
 						</Grid>
 						<Grid item xs={8}>
 							<FormControl fullWidth>
-								<TextField label="Descrição" onChange={handleChange("description")} value={values.description} className={classes.margins} placeholder="Conta de luz" />
+								<Input
+									value={values.amount}
+									onChange={handleChange("amount")}
+									placeholder="Quantidade"
+									className={classes.margins}
+								/>
+							</FormControl>
+						</Grid>
+						<Grid item xs={8}>
+							<FormControl fullWidth>
+								<Input
+									value={values.description}
+									onChange={handleChange("description")}
+									placeholder="Ação"
+									className={classes.margins}
+								/>
 							</FormControl>
 						</Grid>
 						<Grid item xs={8}>
@@ -59,14 +89,31 @@ function App() {
 								</TextField>
 							</FormControl>
 						</Grid>
-
 						<Grid item xs={8}>
 							<Button fullWidth className={classes.margins} onClick={handleSubmit}>Adicionar</Button>
 						</Grid>
 					</Grid>
 				</Paper>
 			</Grid>
-			<Grid item xs={12}></Grid>
+			{data.map(items => {
+				return (
+					<Grid container justify="center" className={classes.root} alignItems="center">
+						<Grid item xs={5}>
+							<Paper elevation={2}>
+								<Typography align="center" gutterBottom variant="h6" className={classes.margins}>{items.description}</Typography>
+								<Grid container justify="center" alignItems="center">
+									<Grid item xs={8}>
+										<Typography>Tipo da Ordem: {items.type === 1 ? "Compra" : "Venda"}</Typography>
+										<Typography>Quantidade: {items.amount}</Typography>
+										<Typography>Valor: {items.price}</Typography>
+										<Typography>Data: {moment(items.date).format("DD/MM/YYYY")}</Typography>
+									</Grid>
+								</Grid>
+							</Paper>
+						</Grid>
+					</Grid>
+				)
+			})}
 		</Grid>
 	);
 }
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const { v4: uuidv4 } = require('uuid');
+const moment = require('moment');
 
 mongoose.connect('mongodb+srv://mysavings:pfQmiYJD9vKlSvmk@cluster0-vmm7w.gcp.mongodb.net/mysavings?retryWrites=true&w=majority', { useUnifiedTopology: true, useNewUrlParser: true }); // Iniciando o Database
 
@@ -10,7 +11,8 @@ const savingDataSchema = new mongoose.Schema({
     description: String,
     date: Date,
     amount: Number,
-    type: Number
+    type: Number,
+    price: Number
 }); //Cria o schema no banco (semelhante a criação de um objeto "tabela")
 
 const savings = mongoose.model("savings", savingDataSchema); //indica em qual collection do mongoDB o Schema será salvo
@@ -27,16 +29,23 @@ app.use(express.json());
 app.get("/", (req, res) => res.send("Ok!"))
 
 app.post("/newData", (req, res) => {
+    const { price, amount, type, description } = req.body;
     let Saving = new savings({
         _id: uuidv4(),
-        description: req.body.description,
-        date: Date.now(),
-        amount: req.body.amount,
-        type: req.body.type
+        date: moment.now(),
+        price,
+        amount,
+        type,
+        description
     });
     console.log(Saving);
     Saving.save().then(() => res.json({ message: "Conta Salva", _id: Saving._id })).catch(err => res.json({ message: `Falha no salvamento: ${err}` }))
 });
+
+app.post("/getAllData", async (req,res) => {
+    let allSavings = await savings.find({})
+    res.json({allSavingsLog: allSavings})
+})
 
 const PORT = process.env.PORT || 3001;
 
